8fab47bf641d9c43c5f875febab420dd982c1ca1
Daniel Madridano <8.ddedaniel@gmail.com>
2023-01-20 14:31:24 +0100

    Proyecto finalizado v1

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ const conteo = () => {
     var palabraIntroducida = palabra.value
     if (palabraIntroducida == "") {
         pResultado.innerText = "No has introducido ninguna palabra"
+        pResultado.style="color:darkred"
     } else {
         let oRegex = /o|ó|ò|ô/gi
         let oArray = palabraIntroducida.match(oRegex)
         if (oArray == null) {
             pResultado.innerText = "Ho hay ninguna O"
+            pResultado.style="color:darkred"
         } else {
             let oEnPalabra = oArray.length
             pResultado.innerText = `La palabra tiene ${oEnPalabra} O`
+            pResultado.style="color:black"
         }
     }
 }
